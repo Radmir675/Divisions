2ef576b2f8ed77cc7d8c266d647f018766c76555
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2025-10-04 08:00:37 +0300

    добавил serilog

--- Divisions/src/Devisions.Web/Program.cs
+++ Divisions/src/Devisions.Web/Program.cs
@@ -1,17 +1,25 @@
 using Devisions.Infrastructure.Postgres;
 using Devisions.Web;
 using Devisions.Web.Middlewares;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<AppDbContext>(_ =>
     new AppDbContext(builder.Configuration.GetConnectionString("DefaultConnection")!));
 
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
 builder.Services.AddProgramDependencies();
 
 var app = builder.Build();
-
 app.UseExceptionMiddleware();
+app.UseSerilogRequestLogging();
+
 
 if (app.Environment.IsDevelopment())
 {
