3a8cf7c164c731b3b665612a2861f63906ade96a
Радмир <104122149+Radmir675@users.noreply.github.com>
2025-10-20 20:51:00 +0300

    Dapper

---
+++ Divisions/src/Devisions.Infrastructure.Postgres/NpgSqlRepository/NpgSQlLocationRepository.cs
@@ -0,0 +1,34 @@
+﻿using CSharpFunctionalExtensions;
+using Dapper;
+using Devisions.Application.Locations;
+using Devisions.Domain.Location;
+using Shared.Errors;
+
+namespace Devisions.Infrastructure.Postgres.NpgSqlRepository;
+
+public class NpgSQlLocationRepository : ILocationRepository
+{
+    private readonly NpgSqlConnectionFactory _connectionFactory;
+
+    public NpgSQlLocationRepository(NpgSqlConnectionFactory connectionFactory)
+    {
+        _connectionFactory = connectionFactory;
+    }
+
+    public Task<Result<Guid, Error>> AddAsync(Location location, CancellationToken cancellationToken) =>
+        throw new NotImplementedException();
+
+    public async Task<Result<bool, Error>> ExistsByIdsAsync(
+        IEnumerable<LocationId> locationsId,
+        CancellationToken cancellationToken)
+    {
+        using var connection = await _connectionFactory.GetConnectionAsync();
+        
+        const string sqlQuery= ""SELECT* FROM  """;
+        connection.ExecuteAsync()
+
+    }
+
+    public Task<Result<IEnumerable<Location>, Error>> GetByIdsAsync(IEnumerable<LocationId> locationsId,
+        CancellationToken cancellationToken) => throw new NotImplementedException();
+}
---
+++ Divisions/src/Devisions.Infrastructure.Postgres/NpgSqlRepository/NpgSqlConnectionFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
+namespace Devisions.Infrastructure.Postgres.NpgSqlRepository;
+
+public class NpgSqlConnectionFactory : IDisposable, IAsyncDisposable
+{
+    private readonly NpgsqlDataSource _dataSourse;
+
+    public NpgSqlConnectionFactory(IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+        dataSourceBuilder.UseLoggerFactory(ConsoleDBLogger());
+        _dataSourse = dataSourceBuilder.Build();
+    }
+
+    public async Task<IDbConnection> GetConnectionAsync()
+    {
+        return await _dataSourse.OpenConnectionAsync();
+    }
+
+    private ILoggerFactory ConsoleDBLogger() =>
+        LoggerFactory.Create(builder => builder.AddConsole());
+
+    public void Dispose() => _dataSourse.Dispose();
+
+    public async ValueTask DisposeAsync() => await _dataSourse.DisposeAsync();
+}
